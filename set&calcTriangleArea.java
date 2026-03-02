d59f122b395c5218f65c6c8f2a5cc7d967ea1a43
gshiladakis <66285313+gshiladakis@users.noreply.github.com>
2026-03-02 13:25:15 -0500

    Create set&calcTriangleArea.java

---
+++ set&calcTriangleArea.java
@@ -0,0 +1,10 @@
+public double setTriangleArea() {
+  // Create the variable triangle and set it's value by calling calcTriangleArea with base 20 and height 10
+  double triangle = calcTriangleArea(20, 10);
+  return traingle;
+}
+
+// There's no need to modify calcTriangleArea, just call it in setTriangleArea above 
+public double calcTriangleArea(int base, int height) {
+  return 0.5 * base * height;
+} 
