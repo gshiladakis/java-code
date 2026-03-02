afb5d3cdfbf4947448092710a6b8455f7c17db64
gshiladakis <66285313+gshiladakis@users.noreply.github.com>
2026-03-02 13:39:58 -0500

    Add files via upload

---
+++ welcome (1).java
@@ -0,0 +1,6 @@
+public String welcome() {
+  //Write code here, view the printed output in the panel below
+  String welcomeString = "Welcome to 15-110";
+  System.out.println(welcomeString);
+  return "Welcome to 15-110";
+} 
