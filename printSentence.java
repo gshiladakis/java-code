89a72980b89f258b71daab8840cf3d01968dfb2a
gshiladakis <66285313+gshiladakis@users.noreply.github.com>
2026-03-02 13:45:32 -0500

    Create printSentence.java

---
+++ printSentence.java
@@ -0,0 +1,6 @@
+public String printSentence(String str) {
+    //Write code here, be sure to concatenate the string from the instructions with the str parameter
+    String sentence = "The welcome message is " + str;
+    System.out.println(sentence);
+    return sentence; 
+} 
